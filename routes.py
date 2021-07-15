892d04c5628ff22b2d5b578af7cd24aa7c2b6b7d
David Drost <daviddrostcode@gmail.com>
2021-07-15 02:26:17 -0500

    html_table assignment

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
-from flask import Flask  
+from flask import Flask, render_template
 app = Flask(__name__)    
+
 @app.route('/')          
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 @app.route('/success')
 def success():
@@ -10,13 +11,13 @@ def success():
 
 @app.route('/hello/<string:name>/<int:num>')
 def hello(name,num):
-    return f"hello {name * num} "
+    return render_template('hello.html',name=name,num=num)
 
 @app.route('/dojo')
 def dojo():
     return "Dojo"
 
-    
+
 
 if __name__=="__main__":    
     app.run(debug=True)    
